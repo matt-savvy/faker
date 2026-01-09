8caa9a7e72c7cbbeb1a401e56f2ee7cca796716c
Matt Savoia <matt@1-800-rad-dude.com>
2026-01-09 14:50:29 -0500

    Update deps, remove mix_test_watch
    mix_test_watch's latest version seems incompatible with
    Elixir 1.19

--- mix.exs
+++ mix.exs
@@ -16,7 +16,6 @@ defmodule Faker.Mixfile do
       docs: docs(),
       source_url: @source_url,
       homepage_url: @source_url,
-      cli: cli(),
       dialyzer: [
         flags: [
           :error_handling,
@@ -45,18 +44,13 @@ defmodule Faker.Mixfile do
 
   defp deps do
     [
-      {:credo, "== 1.7.11", only: [:dev, :test], runtime: false},
-      {:dialyxir, "== 1.4.5", only: [:dev], runtime: false},
-      {:earmark, "== 1.4.47", only: :dev, runtime: false},
-      {:ex_doc, "== 0.37.0", only: :dev, runtime: false},
-      {:makeup, "== 1.2.1"},
-      {:makeup_elixir, "== 1.0.1"},
-      {:mix_test_watch, "== 1.2.0", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7.11", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4.5", only: [:dev], runtime: false},
+      {:earmark, "~> 1.4.47", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.37.0", only: :dev, runtime: false},
+      {:makeup, "~> 1.2.1"},
+      {:makeup_elixir, "~> 1.0.1"}
     ]
-  end
-
-  def cli do
-    [preferred_envs: ["test.watch": :test]]
   end
 
   defp docs do
